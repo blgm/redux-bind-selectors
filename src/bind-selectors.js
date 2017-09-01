eb53509c96b1edc2f0a3415ddbbb0c9bcbed533e
George Blue <blgm@users.noreply.github.com>
2017-09-01 12:29:22 +0100

    Do not allow retrospective changes to the selector map (#17)

--- src/bind-selectors.js
+++ src/bind-selectors.js
@@ -8,25 +8,38 @@
  * and merges the results back into a shallow clone of the state object, without polluting the original.
  */
 
-export default function bindSelectors (selectorMap = {}) {
+export default function bindSelectors (inputSelectorMap = {}) {
+  // Check the validity of the selector map object
+  if (typeof inputSelectorMap !== 'object') {
+    throw new Error('The selector map must be specified as an object')
+  }
+  const selectorMap = {...inputSelectorMap}
+
+  // Check that all the selectors are functions
+  for (const key in selectorMap) {
+    if (typeof selectorMap[key] !== 'function') {
+      throw new Error(`The selector '${key}' must be a function`)
+    }
+  }
+
   return (createStore) => (reducer, preloadedState, enhancer) => {
     const store = createStore(reducer, preloadedState, enhancer)
-    let lastStoreState = store.getState()
-    let computedState
 
-    // Check the validity of the selector map
-    if (typeof selectorMap !== 'object') {
-      throw new Error('The selector map must be specified as an object')
+    // Check the initial state
+    let lastStoreState = store.getState()
+    const stateType = typeof lastStoreState
+    if (typeof lastStoreState !== 'object') {
+      throw new Error(`The state must be a JavaScript object, not a '${stateType}'`)
     }
+
+    // Check for duplicate keys between the selector map and the initial state
     for (const key in selectorMap) {
       if (key in lastStoreState) {
         throw new Error(`The selector key '${key}' cannot be used because it exists in the initial state`)
       }
-      if (typeof selectorMap[key] !== 'function') {
-        throw new Error(`The selector '${key}' must be a function`)
-      }
     }
 
+    let computedState
     const getState = () => {
       const currentStoreState = store.getState()
       // Only recalculate the state if the store state has changed, or we have not computed it before
